b9e55aa56f70aa7cc3f17485971a93f91f8e9da0
rickytorrez <torrez.ricky@ymail.com>
2018-04-02 11:25:06 -0400

    User Callbacks

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,18 @@ const keys              = require('../config/keys');
 
 const User              = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+});
+
+
 /******     PASSPORT SET UP FOR GOOGLE     *****/
 /******     STRATEGY     *****/
 passport.use(
@@ -14,7 +26,16 @@ passport.use(
         callbackURL:    '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        new User ({ googleId: profile.id }).save()                                 // Creates an instance of a user and saves to the database
+        User.findOne({ googleId: profile.id })                                      // Look through users collection, find the first record of a googleID equals to the profileID
+            .then((existingUser) => {                                               // Model instance that represents if a user was found
+                if (existingUser) {                                                 // We already have a record with the given profileID
+                    done(null, existingUser);
+                } else {                                                            // We don't have a user record with this ID, make a new record 
+                    new User ({ googleId: profile.id })                             // Creates a new model instance of a user and 
+                        .save()                                                     // Saves to the database
+                        .then(user => done(null, user))
+                }
+            });
         }
     )
 );
