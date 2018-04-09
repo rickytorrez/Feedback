692a730ebde42b4d6cab8ad1db79bfd391602a00
rickytorrez <torrez.ricky@ymail.com>
2018-04-08 21:15:30 -0400

    AXIOS API

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 
-// AXIOS API CHECKING FOR CURRENT USER
+//  AXIOS API CHECKING FOR CURRENT USER
 const fetchUser = () => {
     axios.get('/api/current_user');
 };
